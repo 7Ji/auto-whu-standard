eb031b01e58431ad8c0ae2d7ba32114b8f9d3f09
7Ji <pugokushin@gmail.com>
2021-03-17 16:18:57 +0800

    First commit
    Created the main code base

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # auto-whu-standard
-Auto WHU for standard linux distribution i.e. Arch Linux, Ubuntu, etc. With systemd in mind, this version is much more concise than the openwrt version.
+Auto WHU for standard linux distribution i.e. Arch Linux, Ubuntu, etc. With systemd in mind, this version is much more concise than [the openwrt version](https://github.com/7Ji/auto-whu-openwrt).  
+为标准Linux发行版（如Arch Linux，Ubuntu，Debian等）准备的武大校园网自动登录管理程序，依托于systemd进行后台管理和自动启动以及日志记录，如果计划于在OpenWrt上使用，请使用依赖于SysVinit、和procd等的[OpenWrt版本](https://github.com/7Ji/auto-whu-openwrt)
+
+如何使用？
+--
+0、确保你使用的发行版的init为systemd，你可以用以下命令确定
+````
+systemctl --version
+````
+1、确保curl、sed、grep和bash已安装且grep支持-oP参数，curl支持-d参数，（部分系统可能只内置了不支持这些参数的busybox版，如果发现不支持，那么你应当安装对应的完整版）你可以用以下命令确定
+````
+curl --help
+grep --help
+sed --version
+bash --version
+````
+2、下载auto-whu.sh并将之放置到/usr/sbin/下，并为之增加执行权限，（亦可以手动复制文本并创建此文件）
+````
+wget https://raw.githubusercontent.com/7Ji/auto-whu-standard/main/auto-whu.sh -O /usr/sbin/auto-whu.sh
+chmod +x /usr/sbin/auto-whu.sh
+````
+3、下载auto-whu.service与auto-whu.timer，将之放到/etc/systemd/system/下（亦可以手动复制文本并创建此文件）
+````
+wget https://raw.githubusercontent.com/7Ji/auto-whu-standard/main/auto-whu.service -O /etc/systemd/system/auto-whu.service
+wget https://raw.githubusercontent.com/7Ji/auto-whu-standard/main/auto-whu.timer -O /etc/systemd/system/auto-whu.timer
+````
+4、下载auto-whu.conf，将之放到/etc/下（亦可以手动复制文本并创建此文件）：
+````
+wget https://raw.githubusercontent.com/7Ji/auto-whu-standard/main/auto-whu.conf -O /etc/auto-whu.conf
+````
+5、参照auto-whu.conf.sample，对auto-whu.conf进行修改
+````
+vi /etc/auto-whu.conf
+````
+6、启动auto-whu的自动启动
+````
+systemctl enable --now auto-whu.timer
+````
---
+++ auto-whu.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+# Online check
+check_online() {
+    ping -w1 -W1 -c 1 baidu.com 1>/dev/null 2>&1 
+    [[ $? = 0 ]] && echo "Network is already up" && return 0
+    return 1
+}
+# Check online and immediately exit if is running by systemd
+check_online && [[ $? = 0 ]] && [[ ! -z "$INVOCATION_ID" ]] && exit
+echo "Warning: running auto-whu when already online is dangerous, you may get your account banned for too many login requests. Use systemd and the bundled service and timer file to manage auto-whu instead. Check the repo for more info: https://github.com/7Ji/auto-whu-standard"
+# Help message
+help () {
+    echo "Usage: $0 -u [username] -p [password] -n [network] -m [manual network] -u [url] -c [config file] -f -s -h"
+    echo "      -u username, should be a number of 13 digits"
+    echo "      -p password, any value not empty"
+    echo "      -n network, single-digit number from 0 to 3, 0 for CERNET, 1 for China Telcom, 2 for China Unicom, 3 for China Mobile"
+    echo "      -m a manually specified network name, replace the -n option"
+    echo "      -c config file, path to the configuration file"
+    echo "      -u eportal authorization URL, DO NOT SET IT unless you totally understand it"
+    echo "      -f foreground mode, ignore the systemd check"
+    echo "      -s skip check for sanity for username, password and network"
+    echo "      -h print this message"
+    echo "      *notice that all other arguments will overwrite the value provided by the config file"
+}
+# Check arguments
+[[ $# = 0 ]] && help && exit
+while [[ $# -ge 1 ]]; do
+    if [[ "$1" = '-u' ]]; then
+        ARG_USERNAME="$2"
+        shift
+    elif [[ "$1" = '-p' ]]; then
+        ARG_PASSWORD="$2"
+        shift
+    elif [[ "$1" = '-n' ]]; then 
+        ARG_NETWORK="$2"
+        shift
+    elif [[ "$1" = '-m' ]]; then
+        ARG_NETWORK_MANUAL="$2"
+        shift
+    elif [[ "$1" = '-u' ]]; then
+        ARG_URL="$2"
+        shift
+    elif [[ "$1" = '-c' ]]; then
+        ARG_CONFIG="$2"
+        shift
+    elif [[ "$1" = '-f' ]]; then
+        ARG_IGNORE_SYSTEMD='1'
+    elif [[ "$1" = '-s' ]]; then
+        ARG_IGNORE_SANITY='1'
+    elif [[ "$1" = '-h' ]]; then
+        help && exit
+    fi
+    shift
+done    
+# Check and read configuration file if neccessary
+if [[ ! -z "$ARG_CONFIG" ]]; then
+    [[ ! -f "$ARG_CONFIG" ]] && echo "ERROR: The configuration file '$ARG_CONFIG' you've provided does not exist."
+    [[ ! -r "$ARG_CONFIG" ]] && echo "ERROR: Not allowed to read the configuration file '$ARG_CONFIG', check your permission"
+    source "$ARG_CONFIG"
+fi
+[[ ! -z "$ARG_USERNAME" ]] && USERNAME=$ARG_USERNAME
+[[ ! -z "$ARG_PASSWORD" ]] && PASSWORD=$ARG_PASSWORD
+[[ ! -z "$ARG_NETWORK" ]] && NETWORK=$ARG_NETWORK
+[[ ! -z "$ARG_NETWORK_MANUAL" ]] && NETWORK_MANUAL=$ARG_NETWORK_MANUAL
+[[ ! -z "$ARG_URL" ]] && URL=$ARG_URL
+[[ ! -z "$ARG_IGNORE_SYSTEMD" ]] && IGNORE_SYSTEMD='1'
+[[ ! -z "$ARG_IGNORE_SANITY" ]] && IGNORE_SANITY='1'
+# Default value downgrading
+[[ -z "$NETWORK" && -z "$NETWORK_MANUAL" ]] && NETWORK='0' && echo "Neither network number nor manual network name was set, defaulting network to 0(CERNET)"
+[[ -z "$URL" ]] && URL='http://172.19.1.9:8080/eportal/InterFace.do?method=login' && echo "Using default eportial authorization URL 'http://172.19.1.9:8080/eportal/InterFace.do?method=login'"
+# Check systemd
+if [[ -z "$INVOCATION_ID" && "$IGNORE_SYSTEMD" != 1 ]]; then
+    echo "You are running this script manually or in a non-systemd environment, it's better to manage this script with systemd."
+    echo "Check the github repo to learn how to use this script properly: https://github.com/7Ji/auto-whu-standard"
+    echo "You can set IGNORE_SYSTEMD='1' in the config file or use the argument -f to ignore this check"
+fi
+# Check intergrity or sanity. return code 1 for insanity.
+if [[ "$IGNORE_SANITY" != 1 ]]; then
+    echo "Starting sanity check for username, password and network, you can set IGNORE_SANITY='1' in config file, or use argument -n to ignore this check."
+    [[ ! "$USERNAME" =~ ^[0-9]{13}$ ]] && echo "ERROR:The username '$USERNAME' you provided is not a number of 13 digits" && exit 1
+    [[ -z "$PASSWORD" ]] && echo "ERROR:You've specified an empty password" && exit 1
+    [[ ! "$NETWORK" =~ ^[0-3]$ && -z "$NETWORK_MANUAL" ]] && echo "ERROR:You've specified a network number not supported, only 0-3 is supported, 0 for CERNET(default), 1 for China Telcom, 2 for China Unicom, 3 for China Mobile" && exit 1
+    echo "Sanity check pass."
+fi
+# Network number conversion
+if [[ -z "$NETWORK_MANUAL" ]]; then
+    if [[ "$NETWORK" = 0 ]]; then
+        NETWORK_STRING=Internet
+    elif [[ "$NETWORK" = 1 ]]; then
+        NETWORK_STRING=dianxin
+    elif [[ "$NETWORK" = 2 ]]; then
+        NETWORK_STRING=liantong
+    else   
+        NETWORK_STRING=yidong
+    fi
+else
+    NETWORK_STRING=$NETWORK_MANUAL
+fi
+# Authorization
+echo "Trying to authorize..."
+curl -d "userId=$USERNAME&password=$PASSWORD&service=$NETWORK_STRING&queryString=`curl baidu.com | grep -oP "(?<=\?).*(?=\')" | sed 's/&/%2526/g' | sed 's/=/%253D/g'`&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false" $URL 1>/dev/null 2>&1 
+check_online && [[ $? = 0 ]] && exit
+echo "Failed to authorize, you may need to check your account info and credit and network connection"
